948cf115bb00d88e252acbf013bdbf0013aa597f
Aditya <adityaverma2004@gmail.com>
2022-05-31 22:54:41 +0530

    newChanges

--- group-52-Project--5-project-productsManagementGroup52/src/Route/route.js
+++ group-52-Project--5-project-productsManagementGroup52/src/Route/route.js
@@ -6,22 +6,25 @@ const UserController= require("../controllers/userController")
 const ProductController= require("../controllers/productController")
 const LoginAuth= require("../middleware/middle")
 const CartController= require("../controllers/cartController")
-
+//***********************************USER APIS*******************************************************/
 router.post("/register",UserController.createUser)
 router.post("/login",UserController.login)
 router.get("/user/:userId/profile",LoginAuth.loginCheck, UserController.getUser)
 router.put("/user/:userId/profile", UserController.updateUserDetails)
 
-//*************************************PRODUCT APIS******************************************** */
+//*************************************PRODUCT APIS*********************************************/
 
-router.post("/products",ProductController.createProduct)
+router.post("/products", LoginAuth.loginCheck, ProductController.createProduct)
 router.get("/products/:productId",ProductController.getProductsById)
 router.delete("/products/:productId",ProductController.deleteProduct)
 router.get("/products",ProductController.getByFilter)
 router.put("/products/:productId",ProductController.updateProduct)
 
 //***************************************CART APIS************************************************/
-router.post("/users/:userId/cart",CartController.createCart)
+router.post("/users/:userId/cart",LoginAuth.loginCheck, CartController.createCart)
+router.get("/users/:userId/cart",CartController.getCart)
+router.delete("/users/:userId/cart",CartController.deleteCart)
+router.put("/users/:userId/cart",CartController.updateCart)
 
 
 
--- group-52-Project--5-project-productsManagementGroup52/src/controllers/cartController.js
+++ group-52-Project--5-project-productsManagementGroup52/src/controllers/cartController.js
@@ -92,4 +92,216 @@ const createCart = async function (req, res)
   }
 }
 
+
+const updateCart = async function (req, res) {
+    try {
+  
+        const userId = req.params.userId
+        let data = req.body
+        // let tokenId = req.userId
+  
+        let { cartId, productId, removeProduct } = data
+        
+        // user validation
+        if(!Validation.isValid(userId)){
+            return res.status(400).send({status : false, message : "user Id is missing in length"})
+        }
+  
+        if (!Validation.isValidObjectId(userId)) {
+            return res.status(400).send({ status: false, message: "user id is not valid" })
+        }
+        const validUser = await userModel.findById(userId);
+        if (!validUser) {
+            return res.status(404).send({ status: false, message: "User not present" })
+        }
+         //Authorisation
+        // if (tokenId !== userId) {
+        //     return res.status(403).send({ status: false, message: "Unauthorized user" })
+        // }
+  
+        // checking data in request body
+  
+        if (!Validation.isValidRequestBody(data)) {
+            return res.status(400).send({ status: false, message: "Please enter details to update the document" })
+  
+        }
+        // cart validation
+        
+        if(!cartId){
+            return res.status(400).send({status : flase, message : "cart id is a required field"})
+        }
+  
+        if (!Validation.isValid(cartId)) {
+            return res.status(400).send({ status: false, message: "cart id is missing in length" })
+        }
+  
+        if (!Validation.isValidObjectId(cartId)) {
+            return res.status(400).send({ status: false, message: "cart id is not valid" })
+        }
+  
+        const validCart = await cartModel.findOne({ _id: cartId, userId: userId });
+        if (!validCart) {
+            return res.status(404).send({ status: false, message: "Cart with this parameters not present" })
+        }
+        // product validation
+  
+        if(!productId){
+            return res.status(400).send({status : false, message : "Product id is required to this action"})
+        }
+  
+        if (!Validation.isValid(productId)) {
+            return res.status(400).send({ status: false, message: "product id is missing in length" })
+        }
+  
+        if (!Validation.isValidObjectId(productId)) {
+            return res.status(400).send({ status: false, message: "product id is not valid" })
+        }
+    
+        const validProduct = await productModel.findOne({ _id: productId, isDeleted: false });
+        if (!validProduct) {
+            return res.status(404).send({ status: false, message: "Product not present" })
+        }
+  
+        let items = validCart.items
+        
+        let productArr = items.filter(x => x.productId.toString() == productId)
+        
+        if (productArr.length == 0) {
+            return res.status(404).send({ status: false, message: "Product is not present in cart" })
+        }
+  
+        let index = items.indexOf(productArr[0])
+  
+        // if(!removeProduct){
+        //     return res.status(400).send({ status: false, message: "remove Product is a required field" })
+        // }
+  
+        if (!Validation.isValid(removeProduct)) {
+            return res.status(400).send({ status: false, message: "Please enter removeProduct is missing in length" })
+        }
+  
+        if (!([0, 1].includes(removeProduct))) {
+            return res.status(400).send({ status: false, message: "RemoveProduct field can have only 0 or 1 value" })
+        }
+  
+  
+        if (removeProduct == 0) {
+            
+            validCart.totalPrice = (validCart.totalPrice - (validProduct.price * validCart.items[index].quantity)).toFixed(2)
+            validCart.items.splice(index, 1)
+  
+            validCart.totalItems = validCart.items.length
+            validCart.save()
+  
+        }
+  
+        if (removeProduct == 1) {
+          
+            validCart.items[index].quantity -= 1
+            validCart.totalPrice = (validCart.totalPrice - validProduct.price).toFixed(2)
+            
+            if (validCart.items[index].quantity == 0) {
+                validCart.items.splice(index,1)
+  
+            }
+            validCart.totalItems = validCart.items.length
+  
+            validCart.save()
+  
+        }
+        
+        return res.status(200).send({ status: true, data: validCart })
+  
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+  
+    }
+  }
+
+
+
+
+
+
+
+const getCart = async function (req, res) {
+
+    try {
+        const userId = req.params.userId
+        const jwtUserId = req.userId
+
+        //    authroization 
+
+        // if (!(userId === jwtUserId)) {
+        //     return res.status(400).send({ status: false, msg: "unauthorized access" })
+        // }
+
+        const checkCart = await cartModel.findOne({ userId: userId })
+        if (!checkCart) {
+            return res.status(400).send({ status: false, msg: 'cart does not exist ' })
+        }
+        const checkUser = await userModel.findById(userId)
+        if (!checkUser) {
+            return res.status(400).send({ status: false, msg: 'user does  does not exist ' })
+        }
+
+        res.status(200).send({ status: true, msg: 'sucess', data: checkCart })
+    }
+    catch (error) {
+        res.status(500).send({ status: false, Message: error.message })
+    }
+
+}
+
+
+const deleteCart = async function (req, res) {
+    try {
+        //let userId = req.params.userId
+        let userIdFromParams = req.params.userId
+        let userIdFromToken = req.userId
+        if (! Validation.isValidObjectId(userIdFromParams)) {
+            return res.status(400).send({ status: false, message: "userId is invalid" })
+        }
+
+        const findUserById = await userModel.findOne({ _id: userIdFromParams})
+
+        if (!findUserById) {
+            return res.status(404).send({ status: false, message: "No user found" })
+        }
+
+    //     if (userIdFromToken != userIdFromParams) {
+    //       return res.status(403).send({status: false,message: "Unauthorized access.",});
+    //   }
+
+        const findCartById = await cartModel.findOne({ userId: userIdFromParams})
+
+        if (!findCartById) {
+            return res.status(404).send({ status: false, message: "No cart Available,Already deleted" })
+        }
+
+
+        const deleteProductData = await cartModel.findOneAndUpdate({ userId: userIdFromParams  },
+            { $set: { items:[],totalItems:0,totalPrice:0} },
+            { new: true })
+
+            await cartModel.findOne({ userId: userIdFromParams })
+
+        return res.status(200).send({ status: true, message: "cart deleted successfullly.",data:deleteProductData })
+
+        
+    }catch (error) {
+    return res.status(500).send({ status: false, message: error.message })
+  }
+}
+
+
+
+
+
 module.exports.createCart=createCart;
+module.exports.getCart=getCart;
+module.exports.deleteCart=deleteCart;
+module.exports.updateCart=updateCart;
+
+
